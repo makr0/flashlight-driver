7c33dca5eb5430afaef776785add385a92997691
makr0 <mdt@mbox>
2013-02-09 00:20:12 +0100

    Support jumper settings for mode selection

--- lux-drv.c
+++ lux-drv.c
@@ -12,7 +12,7 @@
 //
 // TODO: make strobe frequency adjustable and store it
 
-#define outpin 1
+#define outpin PB1
 #define PWM OCR0B  //PWM-value
 
 #define adcpin 2
@@ -28,12 +28,20 @@
 //########################################################################################### MODE/PWM SETUP 
 
 //251, 252 are special mode codes handled differently, all other are PWM values
+// dummy, will be copied over
+byte modes[10];
+byte size_modes;
+//PROGMEM byte modes1[]  = { 0 };
+//PROGMEM byte modes2[]  = { 0 };
+//PROGMEM byte modes3[]  = { 0 };
 
-PROGMEM byte modes[]={ 0,      10,50,255,  252,                };
-//                     dummy   main modes, battmode and strobe
+PROGMEM byte modes1[]  = { 0,      13,50,255,  252             }; // used if no jumper set
+PROGMEM byte modes2[]  = { 0,      14,50,255                   }; // used if PB0 low (jumper 2)
+PROGMEM byte modes3[]  = { 0,      255,50,14                   }; // used if PB4 low (jumper 3)
+//                         dummy   main modes, battmode and strobe
 #define LOCKTIME 12 //time in 1/8 s until a mode gets locked.  12/8=1.5s
-//#define BATTMON  125//enable battery monitoring with this threshold
-//#define BATTMON  160 // for testing...
+#define BATTMON  125//enable battery monitoring with this threshold
+//#define BATTMON  211 // for testing...
 
 //###########################################################################################
 
@@ -92,13 +100,23 @@ const uint8_t EEMEM eeprom[64] =
 
 void eepsave(byte data) {  //central method for writing (with wear leveling)
   byte oldpos=eepos;
-  eepos=(eepos+1)&31;  //wear leveling, use next cell
+  eepos=(eepos+1)&15;  //wear leveling, use next cell
   EEARL=eepos; EEDR=data; EECR=32+4; EECR=32+4+2;  //WRITE  //32:write only (no erase)  4:enable  2:go
   while(EECR & 2); //wait for completion
   EEARL=oldpos;           EECR=16+4; EECR=16+4+2;  //ERASE  //16:erase only (no write)  4:enable  2:go
 }
+void eepsave_raw(byte addr, byte data) {  //method for writing at addr without wear leveling
+  EEARL=addr; EEDR=data; EECR=32+4; EECR=32+4+2;  //WRITE  //32:write only (no erase)  4:enable  2:go
+  while(EECR & 2); //wait for completion
+}
 
 
+int jumper2_set(void) {
+    return (PINB & (1 << PB0)) == 0;
+}
+int jumper3_set(void) {
+    return (PINB & (1 << PB4)) == 0;
+}
 
 inline void getmode(void) {  //read current mode from EEPROM and write next mode
 
@@ -107,35 +125,60 @@ inline void getmode(void) {  //read current mode from EEPROM and write next mode
   if (eepos<32) mode=eep[eepos];
   else eepos=0;   //+6  //not found
 
-  byte next=1;
-  if (mode==1) next=2;  //+10
+  byte next=0;
 
   if (mode & 0x80) {  //last on-time was short
-    mode&=0x7f;   if (mode>=sizeof(modes)) mode=1;
-    next=mode+1;  if (next>=sizeof(modes)) next=1; 
-  } //else next=1;  //previous mode was locked, this one is yet a short-on, so set to restart from 1st mode.
+    mode&=0x7f;   if (mode>=size_modes) mode=1;
+    next=mode+1;  if (next>=size_modes) next=1;
+  } else {
+      if( jumper2_set() || jumper3_set() ) {
+          next=1;
+          if (mode==1) next=2;
+      }  // next mode is same as prev
+      else { next=1; }  //previous mode was locked, this one is yet a short-on, so set to restart from 1st mode.
+  }
 
   eepsave(next|0x80); //write next mode, with short-on marker
 }
 
-
-
-
-
 ISR(WDT_vect) {  //WatchDogTimer interrupt
     if (ticks<255) ticks++;
-    if (ticks==LOCKTIME) eepsave(mode);  //lock mode after 1.5s
-
+    if (ticks==LOCKTIME){
+        if( jumper3_set() ) {
+            eepsave( 1 ); // always start in first mode if jumper 3 set
+        } else {
+            eepsave(mode);  //lock mode after 1.5s
+        }
+    }
 
     #ifdef BATTMON //code to check voltage and ramp down
       adcread();
+      eepsave_raw(63,adcresult);
       if (adcresult<BATTMON) { if (++lowbattcounter>8) {PWM=(PWM>>1)+3;lowbattcounter=0;} }
       else lowbattcounter=0;
     #endif
 }
 
-
 int main(void) {
+    byte i=0;
+  if( jumper2_set() ) {
+      for(i=0;i++< sizeof(modes2);) {
+          modes[i]=pgm_read_byte(&modes2[i]);
+      }
+      size_modes = sizeof(modes2);
+  } else {
+      if( jumper3_set() ) {
+          for(i=0;i++< sizeof(modes3);) {
+              modes[i]=pgm_read_byte(&modes3[i]);
+          }
+          size_modes = sizeof(modes3);
+      } else {
+          for(i=0;i++< sizeof(modes1);) {
+              modes[i]=pgm_read_byte(&modes1[i]);
+          }
+          size_modes = sizeof(modes1);
+      }
+  }
 
   portinit();
   sleepinit();
@@ -150,9 +193,7 @@ int main(void) {
 
   getmode();  //get current mode number from EEPROM
 
-  byte i=0;
-
-  pmode=pgm_read_byte(&modes[mode]);  //get actual PWM value (or special mode code)
+  pmode=modes[mode];  //get actual PWM value (or special mode code)
 
 
   switch(pmode){
@@ -206,12 +247,14 @@ int main(void) {
 
 
     default:   mypwm=0;
+              // ramp up to value
                while( mypwm < pmode ) {
                    mypwm++;
                    PWM=mypwm;
-                   _delay_ms(2);
+                   _delay_ms(7);
                }
                while(1){PWM=pmode;SLEEP;}  //all other: use as PWM value
+               break;
 
    //mypwm: prepared for being ramped down in WDT on low-batt condition.
 
